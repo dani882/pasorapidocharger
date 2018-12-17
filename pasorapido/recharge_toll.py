b98b6f67ce73f724a1d5d9b703d0a33990d2aaaf
Jesus Rivera <dani882@gmail.com>
2018-12-16 22:24:53 -0400

    preparing class for automatizacion process

--- pasorapido/recharge_toll.py
+++ pasorapido/recharge_toll.py
@@ -1,1 +1,17 @@
+import time
+from selenium import webdriver
+
 url = "https://pasorapido.gob.do/Account/Login?ReturnUrl=%2Fmicuenta%2Forden"
+
+driver = webdriver.Firefox()
+driver.get(url)
+time.sleep(5)
+
+user_name = driver.find_element_by_name('UserName')
+user_name.send_keys("dani882")
+
+password = driver.find_element_by_name('Password')
+password.send_keys("jesusrivera")
+
+login = driver.find_element_by_class_name('entrar')
+login.submit()
